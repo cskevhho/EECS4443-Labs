05c61a2f05ee7a9faa6d843381e3e2fc34985fec
Kevin <kevhho@my.yorku.ca>
2026-02-06 12:57:17 -0500

    bugfix/small dir clean up, added @+id/main to root of activity_main.xml

--- EECS4443-Lab2/app/src/main/java/com/example/eecs4443_lab2/ui/MainActivity.kt
+++ EECS4443-Lab2/app/src/main/java/com/example/eecs4443_lab2/ui/MainActivity.kt
@@ -10,6 +10,7 @@ import com.example.eecs4443_lab2.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
